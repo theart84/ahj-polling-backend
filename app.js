9446e8b970bb89fab673b76d7ee8c611748d3593
theart84 <artshadow74@gmail.com>
2021-05-22 17:18:21 +0500

    completed

---
+++ app.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const koaBody = require('koa-body');
+const Router = require('koa-router');
+const router = new Router();
+const fakeData = require('./fakerModule')
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({urlencoded: true}))
+
+let messages = [];
+
+router.get('/messages/unread', async (ctx) => {
+  messages.push(fakeData())
+  ctx.response.body = {
+    status: "ok",
+    timestamp: Date.now(),
+    messages: messages
+  };
+})
+
+router.get('/messages/clear', async (ctx) => {
+  messages = [];
+  ctx.response.body = messages;
+})
+
+app.use(router.routes());
+
+module.exports = app;
---
+++ fakerModule.js
@@ -0,0 +1,15 @@
+const faker = require('faker');
+const idGenerator = require('node-unique-id-generator');
+
+
+const fakeData = () => {
+  return {
+    id: idGenerator.generateGUID(),
+    from: faker.internet.email(),
+    subject: faker.lorem.words(),
+    body: faker.lorem.text() ,
+    received: Date.now()
+  }
+}
+
+module.exports = fakeData
